cd9a7c4183ee0cba3e7c3f4e60ca10a01788ed60
Narfsque <h.narof@students.amikom.ac.id>
2025-10-29 10:55:47 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-//#include <limits>
-#include <windows.h>
+#include <iomanip> 
+#include <cmath>   
+#include <string>  
 
 using namespace std;
 
 int main()
 {
-	srand(time(NULL));
 
-	while (true)
-	{
-		int diceRoll = rand() % 6 + 1;
+    double weight_kg, height_m, bmi;
+    string category;
 
-		int guess;
-		cout << "Guess a number between 1 and 6: ";
-		cin >> guess;
-		if (guess == diceRoll)
-		{
-			cout << "Coorrect!" << endl;
-		}
-		else
-		{
-			Beep(656, 1500);
-			cout << "Sorry, the correct number was " << diceRoll << "." << endl;
-		}
-		system("pause");
-	}
-	
-	return 0;
+    cout << "BMI Calculator" << endl;
+    cout << "------------------------------------------" << endl;
+
+    cout << "Weight: ";
+    cin >> weight_kg;
+    cout << "Height: ";
+    cin >> height_m;
+
+    if (cin.fail() || weight_kg <= 0 || height_m <= 0) {
+        cerr << "Input tidak valid. Berat dan tinggi harus angka positif." << endl;
+        return 1; 
+    }
+
+    bmi = weight_kg / (height_m * height_m);
+
+    if (bmi < 18.5) {
+        category = "underweight";
+    }
+    else if (bmi <= 22.99) { 
+        category = "normal";
+    }
+    else if (bmi <= 24.99) { 
+        category = "overweight";
+    }
+    else { 
+        category = "obesity";
+    }
+
+    cout << "\nResult:" << endl;
+    cout << fixed << setprecision(2);
+    cout << "   BMI " << bmi << endl;
+
+    cout << "   You are '" << category << "'" << endl;
+
+    return 0;
 }
-
